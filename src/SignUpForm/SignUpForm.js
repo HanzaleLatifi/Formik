c6ef6f7b8ce290b4a3503d0652da4f83471602b5
h_latifi8 <h.latifi03@gmail.com>
2021-10-10 20:34:28 +0330

    propsgetField & add passwordConfirm

--- src/SignUpForm/SignUpForm.js
+++ src/SignUpForm/SignUpForm.js
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 
 //1 .
 const initialValues={
-    name:"" , email:"" , password:""
+    name:"" , email:"" , password:"" , passwordConfirm:""
 
 }
 //2. 
@@ -15,7 +15,8 @@ const onSubmit=(values)=>{
 const validationSchema=Yup.object({
     name:Yup.string().required('Name is Required') ,
     email:Yup.string().required('Email is required').email('email is invalid') ,
-    password:Yup.string().required('password is Required')
+    password:Yup.string().required('password is Required') ,
+    passwordConfirm:Yup.string().required("password Confirm is required").oneOf([Yup.ref('password'), null], 'Passwords must match')
 
 });
 
@@ -34,20 +35,24 @@ function SignUpForm() {
             <form onSubmit={formik.handleSubmit} className="Form">
                 <div style={{display:"flex" , flexDirection:"column"}}>
                     <label>Name</label>
-                    <input type="text" value={formik.values.name} onBlur={formik.handleBlur} name="name" onChange={formik.handleChange}></input>
+                    <input type="text"  name="name" {...formik.getFieldProps("name")} ></input>
                     {formik.errors.name && formik.touched.name && <div className="error">{formik.errors.name}</div>}
                 </div> 
                 <div style={{display:"flex" , flexDirection:"column"}}>
                     <label>Email</label>
-                    <input type="text" value={formik.values.email} onBlur={formik.handleBlur} name="email" onChange={formik.handleChange}></input>
+                    <input type="text"  name="email" {...formik.getFieldProps("email")} ></input>
                     {formik.errors.email && formik.touched.email && <div className="error">{formik.errors.email}</div>}
 
                 </div> 
                 <div style={{display:"flex" , flexDirection:"column"}}>
                     <label>Password</label>
-                    <input type="text" value={formik.values.password} onBlur={formik.handleBlur} name="password" onChange={formik.handleChange}></input>
+                    <input type="text"  name="password" {...formik.getFieldProps("password")} ></input>
                     {formik.errors.password && formik.touched.password && <div className="error">{formik.errors.password}</div>}
-
+                </div>
+                <div style={{display:"flex" , flexDirection:"column"}}>
+                    <label>Password Confirm</label>
+                    <input type="text"  name="passwordConfirm" {...formik.getFieldProps("passwordConfirm")} ></input>
+                    {formik.errors.passwordConfirm && formik.touched.passwordConfirm && <div className="error">{formik.errors.passwordConfirm}</div>}
                 </div>
                 <button type="submit">Sign Up</button>
             </form>
