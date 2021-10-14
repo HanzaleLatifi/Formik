1dc82b0aa144a103a4ed8c32a186102035c0fdf2
h_latifi8 <h.latifi03@gmail.com>
2021-10-14 12:45:08 +0330

    checkBox term

--- src/SignUpForm/SignUpForm.js
+++ src/SignUpForm/SignUpForm.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import {useFormik} from 'formik'
-import { useState , useEffect } from 'react';
+import { useFormik } from 'formik'
+import { useState, useEffect } from 'react';
 import * as Yup from 'yup';
 import CheckBox from '../Common/CheckBox';
 import Input from '../Common/Input';
@@ -11,39 +11,41 @@ import SelectComponent from '../Common/SelectComponent';
 //1 .
 
 
-const initialValues={
-    name:"" , email:"" , password:"" , passwordConfirm:"" , gender:"" , nationality:"" , intrests:[]
+const initialValues = {
+    name: "", email: "", password: "", passwordConfirm: "", gender: "", nationality: "", intrests: [], terms: false
 
 }
 //2. 
-const onSubmit=(values)=>{
-    console.log(values) 
+const onSubmit = (values) => {
+    console.log(values)
 }
 // 3 .
-const validationSchema=Yup.object({
-    name:Yup.string().required('Name is Required') ,
-    email:Yup.string().required('Email is required').email('email is invalid') ,
-    password:Yup.string().required('password is Required') ,
-    passwordConfirm:Yup.string().required("password Confirm is required").oneOf([Yup.ref('password'), null], 'Passwords must match') ,
-    gender:Yup.string().required("gender Confirm is required") ,
-    nationality:Yup.string().required("select Nationality") ,
-    intrests:Yup.array().min(1).required("required")
+const validationSchema = Yup.object({
+    name: Yup.string().required('Name is Required'),
+    email: Yup.string().required('Email is required').email('email is invalid'),
+    password: Yup.string().required('password is Required'),
+    passwordConfirm: Yup.string().required("password Confirm is required").oneOf([Yup.ref('password'), null], 'Passwords must match'),
+    gender: Yup.string().required("gender Confirm is required"),
+    nationality: Yup.string().required("select Nationality"),
+    intrests: Yup.array().required("required").min(1),
+    terms: Yup.boolean().required().oneOf([true], 'You must accept the terms and conditions.'),
+
 });
 
 //array of obj for map in common compunents
-const radioOptions=[
-    {label:"Male" , value:"1"} ,
-    {label:"female" , value:"0"}
+const radioOptions = [
+    { label: "Male", value: "1" },
+    { label: "female", value: "0" }
 ]
-const selectOptions=[
-    {label:"select Country" , value:"" },
-    {label:"iran" , value:"IR"} ,
-    {label:"japan" , value:"JP"} ,
-    {label:"qatar" , value:"QA"}
+const selectOptions = [
+    { label: "select Country", value: "" },
+    { label: "iran", value: "IR" },
+    { label: "japan", value: "JP" },
+    { label: "qatar", value: "QA" }
 ]
-const checkBox=[
-    {label:"React.js" , value:"react.js"} ,
-    {label:"Vue.js" , value:"vue.js"}
+const checkBox = [
+    { label: "React.js", value: "react.js" },
+    { label: "Vue.js", value: "vue.js" }
 ]
 
 function SignUpForm() {
@@ -51,30 +53,36 @@ function SignUpForm() {
     const [formValues, setFormValues] = useState(null)
 
     useEffect(() => {
-        axios.get("http://localhost:3001/user/1").then((res)=>setFormValues(res.data)).catch((err=>console.log(err)))
-       
+        axios.get("http://localhost:3001/user/1").then((res) => setFormValues(res.data)).catch((err => console.log(err)))
+
     }, [])
 
-   const formik=useFormik({
-       initialValues:formValues || initialValues ,
-       onSubmit ,
-       validationSchema,
-       validateOnMount:true ,
-       enableReinitialize:true
+    const formik = useFormik({
+        initialValues: formValues || initialValues,
+        onSubmit,
+        validationSchema,
+        validateOnMount: true,
+        enableReinitialize: true
 
-   })
-    
+    })
+
     return (
         <>
             <form onSubmit={formik.handleSubmit} className="Form">
-                
+
                 <Input formik={formik} label="Name" name="name" />
                 <Input formik={formik} label="Email" name="email" />
-                <Input formik={formik} label="Password" name="password" type="password"  />
+                <Input formik={formik} label="Password" name="password" type="password" />
                 <Input formik={formik} label="Password Confirm" name="passwordConfirm" type="password" />
-                <RadioInput formik={formik} name="gender" radioOptions={radioOptions}/> 
-                <SelectComponent formik={formik} selectOptions={selectOptions} name="nationality"/>
-                <CheckBox formik={formik} checkBox={checkBox} name="intrests"/>
+                <RadioInput formik={formik} name="gender" radioOptions={radioOptions} />
+                <SelectComponent formik={formik} selectOptions={selectOptions} name="nationality" />
+                <CheckBox formik={formik} checkBox={checkBox} name="intrests" />
+                
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <input type="checkbox" name="terms" value={true} onChange={formik.handleChange}  id="1" checked={formik.values.terms}/>
+                    <label htmlFor="1">terms and conditions</label>
+                    {formik.errors.terms && formik.touched.terms && <div className="error">{formik.errors.terms}</div>}
+                </div>
 
 
                 <button type="submit" disabled={!formik.isValid}>Sign Up</button>
@@ -84,4 +92,4 @@ function SignUpForm() {
     )
 }
 
-    export default SignUpForm
+export default SignUpForm
